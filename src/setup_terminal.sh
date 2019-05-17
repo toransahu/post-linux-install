7b99675eb931fc6d50bb87449a0af19065e749d8
Toran Sahu <toran.sahu@yahoo.com>
2019-05-18 00:38:49 +0530

    zsh plugins

--- src/setup_terminal.sh
+++ src/setup_terminal.sh
@@ -6,10 +6,28 @@
 # Distributed under terms of the MIT license.
 #
 
+mkdir -p ~/.temp
+cd ~/.temp
 
 ## oh-my-zsh
-#-----------
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm -rf $HOME/.oh-my-zsh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# TODO: restore .zshrc
+cp ../../linux-tweaks/backups/.zshrc ~/
+
+# setup zsh as default
+chsh -s $(which zsh)
+
+# install powerline fonts
+sudo apt install tmux fonts-powerline
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 
 ## zsh-autosuggestions
 #------------------
@@ -18,3 +36,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # TODO: fzf
 
 # TODO: Rg 
+
+source ~/.zshrc

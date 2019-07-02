3c1b6d11cf3a78da8c976bc81694bfb79ce68925
Toran Sahu <toran.sahu@yahoo.com>
2019-07-02 16:18:54 +0530

    util packages

---
+++ src/install_util_pkg.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# install_util_pkg.sh
+# Copyright (C) 2019 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# dir tree 
+sudo apt install tree

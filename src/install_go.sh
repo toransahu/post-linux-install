421870b236b7f575208bb6fc04004dcc8c6d9082
Toran Sahu <toran.sahu@yahoo.com>
2020-03-28 18:20:13 +0530

    uninstall old & install go1.14

--- src/install_go.sh
+++ src/install_go.sh
@@ -6,13 +6,18 @@
 # Distributed under terms of the MIT license.
 #
 
+VERSION=1.14
+
+# uninstall existing version
+sudo rm -rf /usr/local/go
+
 mkdir -p ~/.temp
 cd ~/.temp
-wget https://dl.google.com/go/go1.12.5.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.12.5.linux-amd64.tar.gz
+wget https://dl.google.com/go/go$VERSION.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-source ~/.profile
-#echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+# source ~/.profile
+# echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
 
 # for personal repos
 git config --global url."git@github.com:".insteadOf "https://github.com/"

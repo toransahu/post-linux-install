f9f35269594d4583bbd470280a26f619725f3339
Toran Sahu <toran.sahu@yahoo.com>
2020-04-14 23:34:02 +0530

    protobuf  - fetch from latest release

--- src/install_protobuf.sh
+++ src/install_protobuf.sh
@@ -6,35 +6,67 @@
 # Distributed under terms of the MIT license.
 #
 
-# initial step
-mkdir -p  ~/.temp
-cd ~/.temp
 
-# pre-requisites
-sudo apt-get install autoconf automake libtool curl make g++ unzip -y
+get_from_tar(){
+    VERSION=$(curl -s https://api.github.com/repositories/23357588/releases/latest | grep tag_name | cut -d '"' -f 4)
 
-# from this link https://github.com/protocolbuffers/protobuf/releases/tag/v3.6.1 
-# download protobuf-all-[VERSION].tar.gz
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-all-3.6.1.tar.gz
+    # remove old source code - if any
+    rm -rf protobuf-$VERSION
 
-# extract
-tar -xvzf protobuf-all-3.6.1.tar.gz
+    # from this link https://github.com/protocolbuffers/protobuf/releases/tag/v3.6.1 
+    # download protobuf-all-[VERSION].tar.gz
+    wget https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protobuf-all-$VERSION.tar.gz
+    
+    # extract
+    tar -xvzf protobuf-all-$VERSION.tar.gz
+    
+    # change dir
+    cd protobuf-$VERSION/
+}
 
-# change dir
-cd protobuf-3.6.1/
+get_from_git_clone(){
+    # if already exists - pull
+    if [ -d $HOME/.temp/protobuf ]; then
+        # change dir
+        cd protobuf
+        # pull
+        git pull origin master
+    else
+        # get source code from github
+        git clone https://github.com/google/protobuf.git
+    
+        # change dir
+        cd protobuf
+    fi
+}
 
-# configure
-./configure
+install(){
+    # initial step
+    mkdir -p  ~/.temp
+    cd ~/.temp
+    
+    # pre-requisites
+    sudo apt-get install autoconf automake libtool curl make g++ unzip -y
+    
+    # get_from_git_clone
+    get_from_tar
+    
+    # configure
+    ./configure
+    
+    # make
+    make
+    make check
+    
+    # install 
+    sudo make install
+    
+    # refresh shared library cache
+    sudo ldconfig
+}
 
-# make
-make
-make check
-
-# install 
-sudo make install
-
-# refresh shared library cache
-sudo ldconfig
+# install
+install
 
 # validate
 protoc --version

85547c34877a60457562078846be471b9943d414
Toran Sahu <toran.sahu@yahoo.com>
2020-11-12 22:12:16 +0530

    install/upgrade tmux

--- src/setup_tmux.sh
+++ src/setup_tmux.sh
@@ -6,31 +6,51 @@
 # Distributed under terms of the MIT license.
 #
 
+install_update_tmux(){
+    sudo apt update
 
-# install tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    sudo apt install -y git
+    
+    sudo apt install -y automake
+    sudo apt install -y bison
+    sudo apt install -y build-essential
+    sudo apt install -y pkg-config
+    sudo apt install -y libevent-dev
+    sudo apt install -y libncurses5-dev
+    
+    rm -fr /tmp/tmux
+    
+    git clone https://github.com/tmux/tmux.git /tmp/tmux
+    
+    cd /tmp/tmux
+    
+    git checkout master
+    
+    sh autogen.sh
+    
+    ./configure && make
+    
+    sudo make install
+    
+    cd -
+    
+    rm -fr /tmp/tmux
+}
 
-# Put this at the bottom of ~/.tmux.conf ($XDG_CONFIG_HOME/tmux/tmux.conf works too):
+install_tpm(){
+    # install tmux plugin manager
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm  # Reload TMUX environment so TPM is sourced:
+    
+    # type this in terminal if tmux is already running
+    # $ tmux source ~/.tmux.conf
+}
 
-echo "# List of plugins
-set -g @plugin 'tmux-plugins/tpm'
-set -g @plugin 'tmux-plugins/tmux-sensible'
-set -g @plugin 'tmux-plugins/tmux-yank'
+setup(){
+    install_update_tmux
+    install_tpm
+}
 
-# Other examples:
-# set -g @plugin 'github_username/plugin_name'
-# set -g @plugin 'git@github.com/user/plugin'
-# set -g @plugin 'git@bitbucket.com/user/plugin'
-
-# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-run -b '~/.tmux/plugins/tpm/tpm'" >> ~/.tmux.conf
-
-
-
-# Reload TMUX environment so TPM is sourced:
-
-# type this in terminal if tmux is already running
-# $ tmux source ~/.tmux.conf
+setup
 
 ## DOC
 

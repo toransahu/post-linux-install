f64b336a23a41a01a42d79ba81a2440a3c9aba60
Toran Sahu <toran.sahu@yahoo.com>
2020-12-29 20:21:27 +0530

    install/upgrade typora

---
+++ src/install_typora.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# install_typora.sh
+# Copyright (C) 2020 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+# or run:
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+
+# add Typora's repository
+
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+
+sudo apt-get update
+
+# install typora
+
+sudo apt-get install typora

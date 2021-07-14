67fe266649eab704ef9ccb433794dd14566e0fd3
Toran Sahu <toran.sahu@yahoo.com>
2021-07-14 21:16:43 +0530

    install w/o manual intervention

--- src/setup_python.sh
+++ src/setup_python.sh
@@ -11,7 +11,7 @@ cd ~/.temp
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
-sh Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh -b
 
 ## python packages
 #-----------------

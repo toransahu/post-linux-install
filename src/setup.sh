7e18038fa95f8f48baaa369f079d730cc8ba0ec3
Toran Sahu <toran.sahu@yahoo.com>
2021-07-17 19:56:16 +0530

    detect os - wip

--- src/setup.sh
+++ src/setup.sh
@@ -8,6 +8,30 @@
 
 # init
 
+detect_os() {
+    echo "OS detected: $OSTYPE"
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        :
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        :
+    elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+        :
+    elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        :
+    elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+        :
+    elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+        :
+    else
+        echo Unknown OS!
+    fi
+}
+
 # preprocess
 wget -O - https://raw.githubusercontent.com/toransahu/post-linux-install/master/src/preprocessor.sh | sh
 

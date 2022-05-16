02bcc6f7b5b5677267771f276f097897ec5711d4
Toran Sahu <toran.sahu@yahoo.com>
2022-05-16 22:50:00 +0530

    update install_kubectl.sh

--- src/install_kubectl.sh
+++ src/install_kubectl.sh
@@ -8,10 +8,25 @@
 
 
 # recommended way
-cd ~
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+
+via_curl(){
+    cd /tmp
+    # download binary
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    # download checksum
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    # validate
+    echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
+    # if valid, install
+    # if -z $(cat kubectl.sha256) kubectl | sha256sum --check; then
+        sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl;
+    # fi
+    # test
+    kubectl version --client
+}
+
+via_curl
+
 # kubectl version  # test
 
 # using snap 

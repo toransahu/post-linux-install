666ce081e9ee4ec031aaa81fedd6913906145288
Toran Sahu <toran.sahu@yahoo.com>
2023-04-15 11:49:54 +0530

    install postgres

---
+++ src/install_postgres.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# install_postgres.sh
+# Copyright (C) 2022 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+sudo apt-get update -y
+sudo apt-get install postgresql postgresql-contrib -y

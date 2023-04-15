eac8c6bc0b029f70ebc5d60547d4f07a11a1b21e
Toran Sahu <toran.sahu@yahoo.com>
2023-04-15 11:49:35 +0530

    install safe eyes - blink break reminder

---
+++ src/install_safeeyes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# install_safeeyes.sh
+# Copyright (C) 2023 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+sudo apt-get install xprintidle -y
+sudo apt-get install safeeyes -y
